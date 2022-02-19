7cd28c57d54a3515ee0e89f363117db3ad594527
luchho13 <luis.carlos1999h@gmail.com>
2022-02-19 18:06:38 -0500

    Ajustes index y ajax

--- assets/js/ajax.js
+++ assets/js/ajax.js
@@ -3,49 +3,60 @@ $(document).ready(function(){
     var urlPrimerGps = 'https://distanciometro.d2interfaces.com/php/consultar_coordenadas.php?id=0';
     var urlSegundoGps = 'https://distanciometro.d2interfaces.com/php/consultar_coordenadas.php?id=1';
 
-    var latitudPrimerGps, longitudPrimerGps, latitudSegundoGps, longitudSegundoGps, latitudDistancia, longitudDistancia;
+    var lat1, lng1, lat2, lng2;
 
     var cont = 0;
-    var pi80 = Math.PI / 180;
-    var r = 6378.10;
+    var estadoAcercamiento = 0; // 0 es igual a sin acercamiento y 1 es igual a en acercamiento
+    var r = 6378137; // Radio de la tierra en metros
 
-    function acercamientosTotales() {
-
-        $.getJSON(urlPrimerGps, function(datos) {
-
-            latitudPrimerGps = pi80 * datos.latitud;
-            longitudPrimerGps = pi80 * datos.longitud;
-
-        })
-
-        $.getJSON(urlSegundoGps, function(datos) {
-            
-            latitudSegundoGps = pi80 * datos.latitud;
-            longitudSegundoGps = pi80 * datos.longitud;
-
-        })
-        
-        console.log('Latitud Primer GPS: ' + latitudPrimerGps + ' Longitud Primer GPS: ' + longitudPrimerGps);
-        console.log('Latitud Segundo GPS: ' + latitudSegundoGps + ' Longitud Segundo GPS: ' + longitudSegundoGps);
-
-        latitudDistancia = latitudSegundoGps - latitudPrimerGps;
-        longitudDistancia = longitudSegundoGps - longitudPrimerGps;
-
-        var a = Math.pow(Math.sin(latitudDistancia / 2), 2) + Math.cos(latitudPrimerGps) * Math.cos(latitudSegundoGps) * Math.pow(Math.sin(longitudDistancia / 2), 2);
-        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-        var km = r * c;
-
-        console.log(km);
-
-        if(km < 0.0015) {
-            cont++;
-        }
-
-        $('#acercamientos_totales').html(cont);
-
+    function aRadianes(coordenada) {
+        return coordenada * Math.PI / 180;
     }
 
-    setInterval(acercamientosTotales, 1000);
+    function calcularDistancia(){
+        $.getJSON(urlPrimerGps, function(datos) { // Obtenemos las coordenadas del primer GPS
+            lat1 = datos.latitud;
+            lng1 = datos.longitud;
+        })
+
+        $.getJSON(urlSegundoGps, function(datos) { // Obtenemos las coordenadas del segundo GPS
+            lat2 = datos.latitud;
+            lng2 = datos.longitud;
+        })
+
+        var diferenciaLat = aRadianes(lat2 - lat1);
+        var diferenciaLng = aRadianes(lng2 - lng1);
+
+        var a = Math.sin(diferenciaLat / 2)
+                *
+                Math.sin(diferenciaLat / 2)
+                +
+                Math.cos(aRadianes(lat1))
+                *
+                Math.cos(aRadianes(lat2))
+                *
+                Math.sin(diferenciaLng / 2)
+                *
+                Math.sin(diferenciaLng / 2);
+
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        var distancia = r * c;
+
+        console.log(distancia);
+
+        if(estadoAcercamiento == 0 && distancia <= 10) {
+            cont++;
+            estadoAcercamiento = 1;
+        } else if(estadoAcercamiento == 1 && distancia > 10) {
+            estadoAcercamiento = 0;
+        }
+
+        console.log(estadoAcercamiento);
+
+        $('#acercamientos_totales').html(cont);
+    }
+
+    setInterval(calcularDistancia, 1000);
 
 });
 
